b526a236b8afa9bb4ecc7e0f51479cb971fc77a3
hacksparr0w <hacksparr0w@protonmail.com>
2023-04-19 23:36:17 +0200

    feat: convergence data

---
+++ ml4md/plot_cutoff_convergence_plots.py
@@ -0,0 +1,125 @@
+import sys
+
+from enum import Enum
+from pathlib import Path
+
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.subplots
+
+from scipy.signal import savgol_filter
+
+import _home
+
+
+def main():
+    args = sys.argv[1:]
+
+    if len(args) != 1:
+        raise ValueError
+
+    output_dir = Path(args[0]).resolve()
+
+    energy_data = np.genfromtxt(
+        output_dir / "energy_cutoff_convergence.csv",
+        delimiter=" ",
+        skip_header=1
+    )
+
+    force_data = np.genfromtxt(
+        output_dir / "force_cutoff_convergence.csv",
+        delimiter=" ",
+        skip_header=1
+    )
+
+    output_file = (
+        output_dir / f"cutoff_convergence.jpg"
+    )
+
+    plot(energy_data, force_data, output_file)
+
+
+def plot(energy_data, force_data, output_file):
+    x = energy_data[:,0]
+
+    fig = plotly.subplots.make_subplots(
+        rows=2,
+        cols=1,
+        shared_xaxes=True,
+        vertical_spacing=0.1
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=x,
+            y=energy_data[:,1],
+            line=dict(width=1.5),
+            showlegend=False
+        ),
+        row=1,
+        col=1
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=x,
+            y=force_data[:,1],
+            line=dict(width=1.5),
+            showlegend=False
+        ),
+        row=2,
+        col=1
+    )
+
+    fig.update_xaxes(
+        showgrid=False,
+        linewidth=1,
+        linecolor="black",
+        mirror="allticks",
+        ticks="inside",
+        showline=True
+    )
+
+    fig.update_xaxes(
+        title_text="Enegy Cutoff [Ry]",
+        col=1,
+        row=2
+    )
+
+    fig.update_yaxes(
+        title_text="Total Energy [eV]",
+        tickformat=".5f",
+        showgrid=False,
+        linewidth=1,
+        linecolor="black",
+        mirror="allticks",
+        ticks="inside",
+        showline=True,
+        row=1,
+        col=1
+    )
+
+    fig.update_yaxes(
+        title_text="Average force [ev/Å]",
+        showgrid=False,
+        linewidth=1,
+        linecolor="black",
+        mirror="allticks",
+        ticks="inside",
+        showline=True,
+        row=2,
+        col=1
+    )
+
+    fig.update_layout(
+        font_color="#000",
+        plot_bgcolor="#fff",
+        margin=dict(l=1, r=1, b=1, t=1)
+    )
+
+    fig.write_image(output_file, scale=8)
+
+
+if __name__ == "__main__":
+    main()
